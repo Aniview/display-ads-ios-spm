6ff34ca85f51b3dafe1b10560f3086c24b3bc51e
Zhanna Moskaliuk <zhanna.moskaliuk@globallogic.com>
2026-09-03 17:29:46 +0200

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+private let artifactVersion = "1.0.0"
+
+let package = Package(
+    name: "display-ads-ios-spm",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "DisplayAdsSDK",
+            targets: ["DisplayAdsSDKTarget"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
+                 from: "11.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "DisplayAdsSDKTarget",
+            dependencies: [
+                .target(name: "DisplayAdsSDK"),
+                .product(name: "GoogleMobileAds",
+                         package: "swift-package-manager-google-mobile-ads"),
+            ],
+            path: "PlaceHolder"
+        ),
+        .binaryTarget(
+            name: "DisplayAdsSDK",
+            url:
+                "https://github.com/Aniview/display-ads-ios-spm/releases/download/v\(artifactVersion)/display-ads-ios-sdk.zip",
+            checksum: "PLACEHOLDER"
+        )
+    ]
+)
---
+++ README.md
@@ -0,0 +1,28 @@
+# DisplayAdsSDK iOS
+
+Display ads SDK for AdPlayerLite iOS.
+
+## Requirements
+ * iOS >= 15.0
+
+## Integration
+
+### Swift Package Manager
+
+Add this repository URL in Xcode:
+```
+https://github.com/Aniview/display-ads-ios-spm.git
+```
+
+### Usage
+
+```swift
+import AdPlayerLite
+import DisplayAdsSDK
+
+// At app launch, register the display ads provider:
+AdPlayer.setDisplayProvider(DisplayAdsProvider())
+```
+
+## Sample project
+[AdPlayerLiteSample](https://github.com/Aniview/ad-player-lite-ios-example)
